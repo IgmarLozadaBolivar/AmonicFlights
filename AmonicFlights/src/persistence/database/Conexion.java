07de466199e856c7bb8618af1a5f310d42144099
losad <losad@DevWS.bbrouter>
2024-12-11 17:10:00 -0500

    Clase estática de conexión a la base de datos

---
+++ AmonicFlights/src/persistence/database/Conexion.java
@@ -0,0 +1,28 @@
+package persistence.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    public static Connection getConexion() {
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException cnfEx) {
+            System.out.println("Error no se encuentra la clase, más detalles: " + cnfEx.getMessage());
+        }
+        
+        Connection conn = null;
+        
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://44.199.201.71:3306/session1", 
+                    "root", "0000");
+        } catch (SQLException sqlEx) {
+            System.out.println("Error al conectar la base de datos, más detalles: " + sqlEx.getMessage());
+        }
+        
+        return conn;
+    }
+}
